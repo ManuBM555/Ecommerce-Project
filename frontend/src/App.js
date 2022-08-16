74146b0a67efefa8628712527eef903b773a6b0d
ManuBM555 <bmmanu555@gmail.com>
2022-08-16 21:43:59 +0530

    Added UpdatePassword, ForgotPassword, ResetPassword Pages
    Sending mail is working, resetPassword through Mail is working.  9:48

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,9 @@ import { useSelector } from 'react-redux';
 import Profile from "./component/User/Profile.js"
 import ProtectedRoute from './component/Route/ProtectedRoute';
 import UpdateProfile from "./component/User/UpdateProfile.js"
+import UpdatePassword from "./component/User/UpdatePassword.js"
+import ForgotPassword from "./component/User/ForgotPassword.js"
+import ResetPassword from "./component/User/ResetPassword.js"
 
 
 
@@ -57,6 +60,12 @@ function App() {
       <ProtectedRoute exact path='/account' component={Profile} />
 
       <ProtectedRoute exact path='/me/update' component={UpdateProfile} />
+
+      <ProtectedRoute exact path='/password/update' component={UpdatePassword} />
+
+      <Route exact path='/password/forgot' component={ForgotPassword} />
+
+      <Route exact path='/password/reset/:token' component={ResetPassword} />
 
       <Route exact path='/login' component={LoginSignUp} />
 
